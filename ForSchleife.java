0f247f3570e7ce441d92c5976b5aa313ef3e0e4d
Ibrahim Apachi <ibrahim.apachi@icloud.com>
2018-02-22 22:10:17 +0100

    Tutorial für die For Schleife

---
+++ ForSchleife.java
@@ -0,0 +1,11 @@
+public class ForSchleife {
+	public static void main(String[] args) {
+		// Zählervariable; Bedingung; Hochzählung;
+		// Variante1 für das Hochzählen z.B. i = i + 1
+		// Variante2 für das Hochzählen z.B. i += 1
+		// Variante3 für das Hochzählen z.B. i++
+		for (int i = 0; i <= 25; i += 5) {
+			System.out.println(i);
+		}
+	}
+}
