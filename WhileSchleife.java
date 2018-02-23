b5405f748003295f9b3d0b772f1fddea3a519252
Ibrahim Apachi <ibrahim.apachi@icloud.com>
2018-02-23 22:24:49 +0100

    Tutorial für die While-Schleife.

---
+++ WhileSchleife.java
@@ -0,0 +1,14 @@
+public class WhileSchleife {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int zaehler = 0;
+		
+		while (zaehler > -10) {
+			System.out.println(zaehler);
+			zaehler--;
+		}
+	}
+
+}
